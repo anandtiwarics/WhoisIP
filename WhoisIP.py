e267a9c0160dfd24682f18ada104e74d848fbdc9
anand <anandtiwarics@gmail.com>
2017-05-23 12:26:52 +0530

    Exception Handling

--- WhoisIP.py
+++ WhoisIP.py
@@ -1,28 +1,34 @@
 from ipwhois import IPWhois
 import csv
 
-__outFile__ = raw_input("Provide your output File Path with .csv file name: ");
+__outFile__ = raw_input("Provide your output File Path with .csv file name: ")
 
-c = csv.writer(open(__outFile__, "wb"))
-c.writerow(['IP Address','Country','State','City','Description','Name','Emails','Range'])
+try:
+    c = csv.writer(open(__outFile__, "wb"))
+except Exception as error:
+    print "Error in output file", error
+else:
+    c.writerow(['IP Address', 'Country', 'State', 'City', 'Description', 'Name', 'Emails', 'Range'])
 
-__inputFile__ = raw_input("Input your File Path: ");
+__inputFile__ = raw_input("Enter the ip Address: ")
+try:
 
-fileOpne = open( __inputFile__ , 'r')
-lines = fileOpne.read().strip().split()
+    fileOpne = open(__inputFile__, 'r')
+    lines = fileOpne.read().strip().split()
 
-for ip in lines:
-    obj = IPWhois(ip)
-    out = obj.lookup()
+    for ip in lines:
+        obj = IPWhois(ip)
+        out = obj.lookup()
 
-    f = out["nets"][0]['city']
-    a = out["nets"][0]['country']
-    g = out["nets"][0]['description']
-    b = out["nets"][0]['emails']
-    h = out["nets"][0]['name']
-    rangs = out["nets"][0]['range']
-    e = out["nets"][0]['state']
+        f = out["nets"][0]['city']
+        a = out["nets"][0]['country']
+        g = out["nets"][0]['description']
+        b = out["nets"][0]['emails']
+        h = out["nets"][0]['name']
+        rangs = out["nets"][0]['range']
+        e = out["nets"][0]['state']
 
-    c.writerow([ip,a,e,f,g,h,b,rangs])
-
-fileOpne.close()
+        c.writerow([ip, a, e, f, g, h, b, rangs])
+    fileOpne.close()
+except Exception as an:
+    print "Error in input file :", an
